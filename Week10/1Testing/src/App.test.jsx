0e95f69a0203ec6c6ed7689c35118ed68601d293
Anton Rezin <rezin.anton@gmail.com>
2024-09-23 12:15:16 +0300

    Created tests for rendering: header, todotable and add todo functionality.

--- Week10/1Testing/src/App.test.jsx
+++ Week10/1Testing/src/App.test.jsx
@@ -1,5 +1,5 @@
 import { test, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 import App from "./App";
 
@@ -7,4 +7,25 @@ test("renders header", () => {
   render(<App />);
   const headline = screen.getByText(/My Todolist/i);
   expect(headline).toBeInTheDocument();
+});
+
+test("renders todotable", () => {
+  render(<App />);
+  const row = [{ desc: "Go to coffee", date: "24.01.2023" }];
+  const table = screen.getByRole("table");
+  expect(table).not.toHaveTextContent(/go to coffee/i);
+  const button = screen.getByText("Add");
+  fireEvent.click(button);
+});
+
+test("add todo", () => {
+  render(<App />);
+  const desc = screen.getByPlaceholderText("Description");
+  fireEvent.change(desc, { target: { value: "Go to coffee" } });
+  const date = screen.getByPlaceholderText("Date");
+  fireEvent.change(date, { target: { value: "29.01.2023" } });
+  const button = screen.getByText("Add");
+  fireEvent.click(button);
+  const table = screen.getByRole("table");
+  expect(table).toHaveTextContent(/go to coffee/i);
 });
